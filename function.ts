ab133fd7926e7b885220a65489fc18e6aabec6b3
enginepdf <yeindap@gmail.com>
2021-07-21 23:12:12 +0900

    Create function.ts
    function with no return value

---
+++ function.ts
@@ -0,0 +1,8 @@
+function add(item1: number, item2: number): undefined {
+    console.log(item1+item2);
+    return;
+}
+
+function add1(item1: number, item2: number): void {
+    console.log(item1+item2);
+}
